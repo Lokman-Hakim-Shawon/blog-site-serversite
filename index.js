d93e62f9f332921a854d246a6b40c9b5beed88e9
lokman hakim <lokmanhakimshawon3@gmail.com>
2023-11-07 21:52:23 +0600

    create username and password

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ app.use(cors())
 app.use(express.json())
 
 
-// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.trqhxan.mongodb.net/?retryWrites=true&w=majority`;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.trqhxan.mongodb.net/?retryWrites=true&w=majority`;
 
 // // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 // const client = new MongoClient(uri, {
