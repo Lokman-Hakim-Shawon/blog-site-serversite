d2626ea60aab338065f8ea521febfde6c49e4e91
lokman hakim <lokmanhakimshawon3@gmail.com>
2023-11-07 21:50:53 +0600

    create basic express and cors

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-// const express=require('express')
-// const cors=require('cors')
-// require('dotenv').config()
-// const app=express()
-// const port =process.env.PORT || 5000
-// app.use(cors())
-// app.use(express.json())
-
-// // wkL7SQ1pb7x8zMZS
-// // assignment-11
+const express=require('express')
+const cors=require('cors')
+require('dotenv').config()
+const app=express()
+const port =process.env.PORT || 5000
+app.use(cors())
+app.use(express.json())
 
 
 // const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
