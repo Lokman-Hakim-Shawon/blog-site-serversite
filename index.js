2cfef966cdc649ea1b11f792e392b751859f3de1
lokman hakim <lokmanhakimshawon3@gmail.com>
2023-11-07 22:13:20 +0600

    close connect option

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const database = client.db("assignment-11").collection("users");
     const wishlist = client.db("assignment-11").collection("wishlist");
     const comment = client.db("assignment-11").collection("comment");
