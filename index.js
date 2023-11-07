9ac28bf14d711bd9c46b6df3632f91e4524a04dc
lokman hakim <lokmanhakimshawon3@gmail.com>
2023-11-07 22:04:15 +0600

    create delete wishlist data

--- index.js
+++ index.js
@@ -88,12 +88,12 @@ async function run() {
         const result = await wishlist.insertOne(newusers);
         res.send(result)
     })
-    // app.delete('/wishlist_data/:id',async(req,res)=>{
-    //   const id=req.params.id
-    //   const query={_id: new ObjectId(id)}
-    //   const result=await wishlist.deleteOne(query)
-    //   res.send(result)
-    // })
+    app.delete('/wishlist_data/:id',async(req,res)=>{
+      const id=req.params.id
+      const query={_id: new ObjectId(id)}
+      const result=await wishlist.deleteOne(query)
+      res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
